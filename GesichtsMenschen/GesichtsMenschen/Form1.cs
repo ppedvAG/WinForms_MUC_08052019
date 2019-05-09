1689cd5e5179c4c0e935818eef0dd6bc1d786398
André <andrerrr@outlook.de>
2019-05-09 14:37:41 +0200

    Tabluar Rasa

--- GesichtsMenschen/GesichtsMenschen/Form1.cs
+++ GesichtsMenschen/GesichtsMenschen/Form1.cs
@@ -17,19 +17,6 @@ namespace GesichtsMenschen
             InitializeComponent();
         }
 
-        private void employeesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.employeesBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.dataSet1);
 
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // TODO: This line of code loads data into the 'dataSet1.Employees' table. You can move, or remove it, as needed.
-            this.employeesTableAdapter.Fill(this.dataSet1.Employees);
-
-        }
     }
 }
